938600d95f2c0ec210e82f0a87a1e077f04827db
billjyc <793746995@qq.com>
2018-01-30 16:06:21 +0800

    model代码提交

--- snh48/views.py
+++ snh48/views.py
@@ -9,3 +9,7 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+
+def hello(request):
+    return HttpResponse("Hello world")
