257ecb1757a0b1bd308919945806eff8134e4306
billjyc <793746995@qq.com>
2018-01-30 20:51:47 +0800

    template

--- snh48/views.py
+++ snh48/views.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from django.template import loader
+from django.http import Http404
 
 from .models import *
 
@@ -11,9 +13,14 @@ from .models import *
 
 def index(request):
     member_list = Memberinfo.objects.order_by('id')
-    output = ', '.join([member.name for member in member_list])
-    return HttpResponse(output)
+    # template = loader.get_template('snh48/index.html')
+    context = {
+        'member_list': member_list,
+    }
+    return render(request, 'snh48/index.html', context)
+    # return HttpResponse(template.render(context, request))
 
 
 def member_detail(request, member_id):
-    return HttpResponse("You are looking at member %s" % member_id)
+    member = get_object_or_404(Memberinfo, pk=member_id)
+    return render(request, 'snh48/member_detail.html', {'member': member})
