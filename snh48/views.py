009807dfa4a27ec20243c8c302c0354407a1dfc7
billjyc <793746995@qq.com>
2018-01-31 15:55:33 +0800

    首页 对成员列表进行分队

--- snh48/views.py
+++ snh48/views.py
@@ -2,9 +2,6 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
-from django.template import loader
-from django.http import Http404
 
 from .models import *
 
@@ -15,19 +12,27 @@ def index(request):
     member_list = Memberinfo.objects.filter(
         is_valid=1
     ).order_by('id').order_by('team')
-    # template = loader.get_template('snh48/index.html')
     context = {
-        'member_list': member_list,
+        'team_sii': member_list.filter(team__id=1),
+        'team_nii': member_list.filter(team__id=2),
+        'team_hii': member_list.filter(team__id=3),
+        'team_x': member_list.filter(team__id=4),
+        'team_xii': member_list.filter(team__id=5),
     }
     return render(request, 'snh48/index.html', context)
-    # return HttpResponse(template.render(context, request))
 
 
 def member_detail(request, member_id):
+    """
+    获取成员详情
+    :param request:
+    :param member_id: 成员id
+    :return:
+    """
     member = get_object_or_404(Memberinfo, pk=member_id)
     member_performance_history_list = MemberPerformanceHistory.objects.filter(member=member)
     context = {
         'member': member,
-        'mph_list': member_performance_history_list
+        'mph_list': member_performance_history_list,
     }
     return render(request, 'snh48/member_detail.html', context)
