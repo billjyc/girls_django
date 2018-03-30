2dbdfc9d3fff48edf0d4ac34708ddca730acac86
billjyc <793746995@qq.com>
2018-03-30 11:58:17 +0800

    add modian app

--- django_exercise/urls.py
+++ django_exercise/urls.py
@@ -20,5 +20,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^snh48/', include('snh48.urls')),
+    url(r'^modian/', include('modian.urls')),
     url(r'^admin/', admin.site.urls),
 ]
--- modian/views.py
+++ modian/views.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+from django.http import HttpResponse
 
 from django.shortcuts import render
 
+
 # Create your views here.
+def index(request):
+    return HttpResponse("Hello, world. You're at the modian index.")
