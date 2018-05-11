d81463a5da2b90880bfb3399c3d8348a09fe953b
billjyc <793746995@qq.com>
2018-05-11 17:36:50 +0800

    日志模块增加

--- django_exercise/settings.py
+++ django_exercise/settings.py
@@ -181,7 +181,7 @@ LOGGING = {
         'default': {
             'level':'DEBUG',
             'class':'logging.handlers.RotatingFileHandler',
-            'filename': '/logs/all.log',     #日志输出文件
+            'filename': '../logs/all.log',     #日志输出文件
             'maxBytes': 1024*1024*50,                  #文件大小
             'backupCount': 5,                         #备份份数
             'formatter':'standard',                   #使用哪种formatters日志格式
@@ -189,7 +189,7 @@ LOGGING = {
         'error': {
             'level':'ERROR',
             'class':'logging.handlers.RotatingFileHandler',
-            'filename': '/logs/error.log',
+            'filename': '../logs/error.log',
             'maxBytes':1024*1024*5,
             'backupCount': 5,
             'formatter':'standard',
@@ -202,7 +202,7 @@ LOGGING = {
         'request_handler': {
             'level':'DEBUG',
             'class':'logging.handlers.RotatingFileHandler',
-            'filename': '/logs/script.log',
+            'filename': '../logs/script.log',
             'maxBytes': 1024*1024*5,
             'backupCount': 5,
             'formatter':'standard',
@@ -210,7 +210,7 @@ LOGGING = {
         'scripts_handler': {
             'level':'DEBUG',
             'class':'logging.handlers.RotatingFileHandler',
-            'filename':'/logs/script.log',
+            'filename':'../logs/script.log',
             'maxBytes': 1024*1024*5,
             'backupCount': 5,
             'formatter':'standard',
