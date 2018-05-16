f2fe8b552e757f5b195d97bb5817cae9f2b9c2b4
billjyc <793746995@qq.com>
2018-05-16 14:13:58 +0800

    获取姐妹团B站（寒影）播放数据

--- snh48/logic/bilibili_sister_group.py
+++ snh48/logic/bilibili_sister_group.py
@@ -10,20 +10,30 @@ logger = logging.getLogger(__name__)
 
 
 def get_video_stat(keyword):
-    url = 'https://space.bilibili.com/ajax/member/getSubmitVideos?mid=1315101&pagesize=50&tid=0&page=1&keyword=%s&order=pubdate' % keyword
-    header = {
-        'Host': 'space.bilibili.com',
-        'Referer': 'https://space.bilibili.com/1315101/',
-        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36'
-    }
-    r = requests.get(url, headers=header).json()
-    vlist = r['data']['vlist']
-    print(vlist)
+    print(keyword)
+    page = 1
+    result = []
+    while True:
+        url = 'https://space.bilibili.com/ajax/member/getSubmitVideos?mid=1315101&pagesize=100&tid=0&page=%s&keyword=%s&order=pubdate' % (page, keyword)
+        header = {
+            'Host': 'space.bilibili.com',
+            'Referer': 'https://space.bilibili.com/1315101/',
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36'
+        }
+        r = requests.get(url, headers=header).json()
+        vlist = r['data']['vlist']
+        if len(vlist) == 0:
+            break
+        print(vlist)
+        result.extend(vlist)
+        page += 1
+        time.sleep(5)
+
     # 写入csv文件
     with open('%s.csv' % keyword, 'w', encoding='utf8') as f:
         writer = csv.writer(f, dialect=('excel'))
         writer.writerow(['上传时间', '公演', '播放量', '弹幕'])
-        for video in vlist:
+        for video in result:
             # 转换成localtime
             time_local = time.localtime(video['created'])
             # 转换成新的时间格式(2016-05-05 20:28:54)
@@ -33,4 +43,13 @@ def get_video_stat(keyword):
 
 
 if __name__ == '__main__':
-    get_video_stat('Team K')
+    get_video_stat('Team NII')
+    get_video_stat('Team HII')
+    get_video_stat('Team X')
+    get_video_stat('Team XII')
+    get_video_stat('Team B')
+    get_video_stat('Team E')
+    get_video_stat('Team J')
+    get_video_stat('Team G')
+    get_video_stat('Team NIII')
+    get_video_stat('Team Z')
