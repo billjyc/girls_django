1370d8a72a4dc3247dac9663ac6c16a3a364ad89
billjyc <billjyc@gmail.com>
2018-10-15 22:39:26 +0800

    生祝长度修正

--- modian/logic/birthday_form.py
+++ modian/logic/birthday_form.py
@@ -2,8 +2,8 @@ from django import forms
 
 
 class BirthdayForm(forms.Form):
-    user_id = forms.CharField(max_length=30)
+    user_id = forms.CharField(max_length=24)
     province_code = forms.IntegerField()
-    birthday_wish = forms.CharField(max_length=480, widget=forms.Textarea)
+    birthday_wish = forms.CharField(max_length=240, widget=forms.Textarea)
     ip = forms.CharField(max_length=100)
 
