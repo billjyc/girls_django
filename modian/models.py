6dfbebe9cbedacee36d0c2153bdad684422bdb61
billjyc <793746995@qq.com>
2018-12-03 16:06:47 +0800

    查看抽卡记录

--- modian/models.py
+++ modian/models.py
@@ -78,7 +78,8 @@ class Card(models.Model):
 
 class DrawRecord(models.Model):
     supporter = models.ForeignKey('Supporter', models.DO_NOTHING, db_column='supporter_id')
-    card = models.ForeignKey('Card', models.DO_NOTHING, db_column='card_id')
+    # card = models.ForeignKey('Card', models.DO_NOTHING, db_column='card_id')
+    card_id = models.IntegerField(db_column='card_id')
     draw_time = models.DateTimeField()
 
     class Meta:
