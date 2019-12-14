ed380279db57ed6e988186737bfba43c44d6f99a
billjyc <billjyc@gmail.com>
2019-12-14 21:57:36 +0800

    页面优化

--- django_exercise/settings.py
+++ django_exercise/settings.py
@@ -164,6 +164,7 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '/home/girls_static'
 
 FILE_CHARSET = 'utf8'
 DEFAULT_CHARSET = 'utf8'
