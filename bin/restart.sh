ee0c43935f561686a4461742eecb359e9df02c01
billjia <billjia@tencent.com>
2021-01-04 09:51:07 +0800

    启动脚本调整

--- bin/restart.sh
+++ bin/restart.sh
@@ -1,3 +1,4 @@
 ps -eux | grep gunicorn | grep -v grep | awk '{print $2}' | xargs kill -9
-python3 manage.py collectstatic
-BUILD_ID=DONTKILLME nohup gunicorn django_exercise.wsgi:application -c gunicorn_config.py &
+ROOT_DIR=".."
+python3 ${ROOT_DIR}/manage.py collectstatic
+BUILD_ID=DONTKILLME nohup gunicorn django_exercise.wsgi:application -c ${ROOT_DIR}/gunicorn_config.py &
