09052b6ffb97104d57b2a17a46d814d94ae0ac3d
billjyc <billjyc@gmail.com>
2023-04-06 22:57:57 +0800

    修改启动脚本

--- docker_start.sh
+++ docker_start.sh
@@ -1,5 +1,7 @@
 python3 manage.py collectstatic
 mkdir logs
+nohup celery -A django_exercise worker --loglevel=info > logs/celery_worker.log 2>&1 &
+nohup celery -A django_exercise beat --loglevel=info > logs/celery_beat.log 2>&1 &
 gunicorn django_exercise.wsgi:application -c gunicorn_config.py
 
 tail -f /dev/null
