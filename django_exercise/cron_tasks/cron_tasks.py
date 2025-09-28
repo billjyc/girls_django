ab0c8db83ebe36e6d92f853ddc6e74c0332e6c5f
billjyc <jiayicong@vip.qq.com>
2025-09-28 15:59:12 +0800

    replace by geckodriver

--- django_exercise/cron_tasks/cron_tasks.py
+++ django_exercise/cron_tasks/cron_tasks.py
@@ -4,7 +4,7 @@ from celery.schedules import crontab
 CELERY_BEAT_SCHEDULE = {
     'update_weibo_follower_count': {
         'task': 'snh48.tasks.update_weibo_followers_count',
-        'schedule': crontab(minute='33', hour='9'),
+        'schedule': crontab(minute='2', hour='16'),
         'args': (),
         # 'options': {'queue': 'your_queue_name'},
     },
